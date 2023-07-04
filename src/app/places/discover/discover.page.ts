8c607f40b132c1b9422b42be05c29768fbf040a2
mateusmedeir <mateusmlopestr@gmail.com>
2023-07-04 12:40:14 -0300

    feat(discover): add infinite scroll in discover page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../places.model';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -8,12 +9,31 @@ import { Place } from '../places.model';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
-  places: Place[] = [];
+  places!: Place[];
+  loadedPlaces!: Place[];
+  index: number = 1;
+  increasedAmount: number = 10;
 
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
     this.places = this.placesService.places;
+    this.loadedPlaces = this.places.slice(this.index, this.increasedAmount + 1);
+    this.index += this.loadedPlaces.length;
   }
 
+  private loadPlaces() {
+    console.log(this.index, this.index + this.increasedAmount);
+    this.loadedPlaces.push(...this.places.slice(this.index, this.index + this.increasedAmount));
+    this.index += this.increasedAmount + 1;
+  }
+
+  onIonInfinite(ev: any) {
+    setTimeout(() => {
+      this.loadPlaces();
+      (ev as InfiniteScrollCustomEvent).target.complete();
+      if (this.loadedPlaces.length + 1 === this.places.length)
+        (ev as InfiniteScrollCustomEvent).target.disabled = true;
+    }, 500);
+  }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -26,7 +26,112 @@ export class PlacesService {
       'Not your average city trip!',
       'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
       99.99
-    )
+    ),
+    new Place(
+      'p4',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+      149.99
+    ),
+    new Place(
+      'p5',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+      189.99
+    ),
+    new Place(
+      'p6',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+      99.99
+    ),
+    new Place(
+      'p7',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+      149.99
+    ),
+    new Place(
+      'p8',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+      189.99
+    ),
+    new Place(
+      'p9',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+      99.99
+    ),
+    new Place(
+      'p10',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+      149.99
+    ),
+    new Place(
+      'p11',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+      189.99
+    ),
+    new Place(
+      'p12',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+      99.99
+    ),
+    new Place(
+      'p13',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+      149.99
+    ),
+    new Place(
+      'p14',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+      189.99
+    ),
+    new Place(
+      'p15',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+      99.99
+    ),
+    new Place(
+      'p16',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+      149.99
+    ),
+    new Place(
+      'p17',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+      189.99
+    ),
+    new Place(
+      'p18',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+      99.99
+    ),
   ];
 
   get places() {
